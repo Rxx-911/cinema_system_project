2098958e72284c45909ef02c643c5a00bed313cd
R xx <rxx@RdeMacBook-Air-148.local>
2026-04-18 01:48:50 +0100

    restore user controller

---
+++ cinema_backend/src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    private final UserRepository repo;
+
+    public UserController(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    // 获取所有用户
+    @GetMapping
+    public List<UserEntity> getAll() {
+        return repo.findAll();
+    }
+
+    // 注册用户
+    @PostMapping
+    public UserEntity create(@RequestBody UserEntity user) {
+        return repo.save(user);
+    }
+}
